4051938d32ee676610131e821add0a7998759ac4
Kristiana Llanos <llanosengineering@gmail.com>
2025-04-16 20:56:39 -0500

    RESOLVES deployment debugging to llanosengineering.com

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,18 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Ensure the output is in the 'dist' directory
-    rollupOptions: {
-      input: "./src/main.jsx", // Your entry point file
-      output: {
-        dir: "dist", // Output the files in the 'dist' directory
-        entryFileNames: "worker.js", // This will generate a file called 'worker.js'
-        format: "esm", // Cloudflare Workers require ES Modules
-      },
-    },
+    outDir: "dist",
+    emptyOutDir: true,
   },
 });
